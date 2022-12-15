e3b7c146877fc623758d917eff2a19a7c46bbb18
Dan <DanOfTheRoses@gmail.com>
2022-12-15 11:19:08 -0600

    faster webgl2 detection in Chrome

--- src/gl/webgl2.ts
+++ src/gl/webgl2.ts
@@ -3,5 +3,5 @@
 export function isWebGL2(
     gl: WebGLRenderingContext
 ): gl is WebGL2RenderingContext {
-    return gl.getParameter(gl.VERSION).startsWith('WebGL 2.0');
+    return gl.canvas.getContext('webgl2') === gl;
 }
