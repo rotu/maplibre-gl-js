58f43c189d2bb83ff72411eefd2bed3c17be110c
Dan <DanOfTheRoses@gmail.com>
2022-12-21 01:21:48 -0600

    simplyify build config?

--- build/banner.ts
+++ build/banner.ts
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import packageJSON from '../package.json' assert {type: 'json'};
 
-const version = JSON.parse(fs.readFileSync('package.json').toString()).version;
+const version = packageJSON.version;
 export default `/* MapLibre GL JS is licensed under the 3-Clause BSD License. Full text of license: https://github.com/maplibre/maplibre-gl-js/blob/v${version}/LICENSE.txt */`;
