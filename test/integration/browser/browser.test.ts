0e7ce792cc781c16e18b6403a2c3f50c927022f2
Dan <DanOfTheRoses@gmail.com>
2022-12-28 01:46:01 -0600

    cool. Browser tests run on vitest.

--- test/integration/browser/browser.test.ts
+++ test/integration/browser/browser.test.ts
@@ -1,212 +1,172 @@
-import {chromium, firefox, webkit} from 'playwright';
+import pw, {BrowserType, chromium, firefox, webkit} from 'playwright';
 import st from 'st';
-import http from 'http';
+import http from 'node:http';
 import {fileURLToPath} from 'node:url';
-
-const projectRoot = fileURLToPath(new URL('../../..', import.meta.url));
-
 import {toMatchImageSnapshot} from 'jest-image-snapshot';
 expect.extend({toMatchImageSnapshot});
+declare global {
+    namespace jest {
+        interface Matchers<R> {
+            toMatchImageSnapshot(): R;
+        }
+    }
+}
 
-describe('browser tests', () => {
-    let port: number;
-    let browser;
-    let server;
-    let page;
-    let map;
-    let canvas;
+const projectRoot = fileURLToPath(new URL('../../..', import.meta.url));
+const browserTypes = [{
+    name: 'Chromium', browserType: pw.chromium
+}, {
+    name: 'Firefox', browserType: pw.firefox
+}, {
+    name: 'WebKit', browserType: pw.webkit
+}];
 
-    // const thisDir = path.resolve(import.meta.url);
-    // start server
-    beforeAll(async () => {
-        port = await new Promise((resolve) => {
-            server = http.createServer(
-                st(projectRoot)
-            ).listen(() => {
-                resolve(server.address().port);
+describe.each(browserTypes)('browser = $name', ({browserType}) => {
+    describe('browser tests', () => {
+        let port: number;
+        let browser;
+        let server;
+        let page;
+        let map;
+        let canvas;
+
+        // const thisDir = path.resolve(import.meta.url);
+        // start server
+        beforeAll(async () => {
+            port = await new Promise((resolve) => {
+                server = http.createServer(
+                    st(projectRoot)
+                ).listen(() => {
+                    resolve(server.address().port);
+                });
             });
-        });
-        browser = await chromium.launch();
+            browser = await browserType.launch();
 
-    });
-
-    afterAll(async () => {
-        await Promise.allSettled([
-            new Promise((resolve) => server.close(resolve)),
-            () => browser.close()]);
-
-    });
-
-    beforeEach(async () => {
-        page = await browser.newPage({});
-        await page.goto(`http://localhost:${port}/test/integration/browser/fixtures/land.html`);
-        canvas = await page.evaluateHandle(() => new Promise((resolve, reject) => {
-            setTimeout(reject, 1000);
-            if (map.loaded()) {
-                resolve(map.getCanvas());
-            } else {
-                map.once('load', () => resolve(map.getCanvas()));
-            }
-        }));
-    });
-
-    test('Drag to the left', async () => {
-        const canvasBB = await canvas.boundingBox();
-
-        // Perform drag action, wait a bit the end to avoid the momentum mode.
-        await page.mouse.move(canvasBB.x, canvasBB.y);
-        await page.mouse.down();
-        await page.mouse.move(100, 0);
-        await new Promise(r => setTimeout(r, 200));
-        await page.mouse.up();
-
-        const center = await page.evaluate(() => {
-            return map.getCenter();
         });
 
-        expect(center.lng).toBeCloseTo(-35.15625, 4);
-        expect(center.lat).toBeCloseTo(0, 7);
-        expect(await page.screenshot()).toMatchImageSnapshot();
+        afterAll(async () => {
+            await Promise.allSettled([
+                new Promise((resolve) => server.close(resolve)),
+                () => browser?.close()]);
 
-    }, 20000);
+        });
 
-    test('Zoom: Double click at the center', async () => {
-        expect(await page.screenshot()).toMatchImageSnapshot();
+        beforeEach(async () => {
+            page = await browser.newPage({});
+            await page.goto(`http://localhost:${port}/test/integration/browser/fixtures/land.html`);
+            canvas = await page.evaluateHandle(() => new Promise((resolve, reject) => {
+                setTimeout(reject, 1000);
+                if (map.loaded()) {
+                    resolve(map.getCanvas());
+                } else {
+                    map.once('load', () => resolve(map.getCanvas()));
+                }
+            }));
+        });
 
-        const canvasBB = await canvas.boundingBox();
+        test('Drag to the left', async () => {
+            const canvasBB = await canvas.boundingBox();
 
-        await page.mouse.dblclick(canvasBB.x, canvasBB.y);
+            // Perform drag action, wait a bit the end to avoid the momentum mode.
+            await page.mouse.move(canvasBB.x, canvasBB.y);
+            await page.mouse.down();
+            await page.mouse.move(100, 0);
+            await new Promise(r => setTimeout(r, 200));
+            await page.mouse.up();
 
-        // Wait until the map has settled, then report the zoom level back.
-        const zoom = await page.evaluate(() => {
-            return new Promise((resolve, _reject) => {
-                map.once('idle', () => resolve(map.getZoom()));
+            const center = await page.evaluate(() => {
+                return map.getCenter();
             });
+
+            expect(center.lng).toBeCloseTo(-35.15625, 4);
+            expect(center.lat).toBeCloseTo(0, 7);
+            expect(await page.screenshot()).toMatchImageSnapshot();
+        }, 20000);
+
+        test('Zoom: Double click at the center', async () => {
+            expect(await page.screenshot()).toMatchImageSnapshot();
+
+            const canvasBB = await canvas.boundingBox();
+
+            await page.mouse.dblclick(canvasBB.x, canvasBB.y);
+
+            // Wait until the map has settled, then report the zoom level back.
+            const zoom = await page.evaluate(() => {
+                return new Promise((resolve, _reject) => {
+                    map.once('idle', () => resolve(map.getZoom()));
+                });
+            });
+
+            expect(zoom).toBe(2);
+            expect(await page.screenshot()).toMatchImageSnapshot();
+
+        }, 20000);
+
+        test('CJK Characters', async () => {
+            await page.evaluate(() => {
+                map.setStyle({
+                    version: 8,
+                    glyphs: 'https://mierune.github.io/fonts/{fontstack}/{range}.pbf',
+                    sources: {
+                        sample: {
+                            type: 'geojson',
+                            data: {
+                                type: 'Feature',
+                                geometry: {
+                                    type: 'Point',
+                                    coordinates: [0, 0]
+                                },
+                                properties: {
+                                    'name_en': 'abcde',
+                                    'name_ja': 'あいうえお',
+                                    'name_ch': '阿衣乌唉哦',
+                                    'name_kr': '아이우'
+                                }
+                            }
+                        },
+                    },
+                    'layers': [
+                        {
+                            'id': 'sample-text-left',
+                            'type': 'symbol',
+                            'source': 'sample',
+                            'layout': {
+                                'text-anchor': 'top',
+                                'text-field': '{name_ja}{name_en}',
+                                'text-font': ['Open Sans Regular'],
+                                'text-offset': [-10, 0],
+                            }
+                        },
+                        {
+                            'id': 'sample-text-center',
+                            'type': 'symbol',
+                            'source': 'sample',
+                            'layout': {
+                                'text-anchor': 'top',
+                                'text-field': '{name_ch}{name_kr}',
+                                'text-font': ['Open Sans Regular'],
+                                'text-offset': [0, 0],
+                            }
+                        },
+                        {
+                            'id': 'sample-text-right',
+                            'type': 'symbol',
+                            'source': 'sample',
+                            'layout': {
+                                'text-anchor': 'top',
+                                'text-field': '{name_en}{name_ja}',
+                                'text-font': ['Open Sans Regular'],
+                                'text-offset': [10, 0],
+                            }
+                        },
+                    ]
+                });
+                return new Promise((resolve, _) => {
+                    map.once('idle', resolve);
+                });
+            });
+            expect(await page.screenshot()).toMatchImageSnapshot();
         });
-
-        expect(zoom).toBe(2);
-        expect(await page.screenshot()).toMatchImageSnapshot();
-
-    }, 20000);
-
-    // test(`${impl.name()} - CJK Characters`, async () => {
-    //     await newTest(impl);
-    //     await page.evaluate(() => {
-
-    //         map.setStyle({
-    //             version: 8,
-    //             glyphs: 'https://mierune.github.io/fonts/{fontstack}/{range}.pbf',
-    //             sources: {
-    //                 sample: {
-    //                     type: 'geojson',
-    //                     data: {
-    //                         type: 'Feature',
-    //                         geometry: {
-    //                             type: 'Point',
-    //                             coordinates: [0, 0]
-    //                         },
-    //                         properties: {
-    //                             'name_en': 'abcde',
-    //                             'name_ja': 'あいうえお',
-    //                             'name_ch': '阿衣乌唉哦',
-    //                             'name_kr': '아이우'
-    //                         }
-    //                     }
-    //                 },
-    //             },
-    //             'layers': [
-    //                 {
-    //                     'id': 'sample-text-left',
-    //                     'type': 'symbol',
-    //                     'source': 'sample',
-    //                     'layout': {
-    //                         'text-anchor': 'top',
-    //                         'text-field': '{name_ja}{name_en}',
-    //                         'text-font': ['Open Sans Regular'],
-    //                         'text-offset': [-10, 0],
-    //                     }
-    //                 },
-    //                 {
-    //                     'id': 'sample-text-center',
-    //                     'type': 'symbol',
-    //                     'source': 'sample',
-    //                     'layout': {
-    //                         'text-anchor': 'top',
-    //                         'text-field': '{name_ch}{name_kr}',
-    //                         'text-font': ['Open Sans Regular'],
-    //                         'text-offset': [0, 0],
-    //                     }
-    //                 },
-    //                 {
-    //                     'id': 'sample-text-right',
-    //                     'type': 'symbol',
-    //                     'source': 'sample',
-    //                     'layout': {
-    //                         'text-anchor': 'top',
-    //                         'text-field': '{name_en}{name_ja}',
-    //                         'text-font': ['Open Sans Regular'],
-    //                         'text-offset': [10, 0],
-    //                     }
-    //                 },
-    //             ]
-    //         });
-    //     });
+    });
 
 });
-
-//     const image = await page.evaluate(() => {
-//         return new Promise((resolve, _) => {
-//             map.once('idle', () => resolve(map.getCanvas().toDataURL()));
-//             map.setZoom(8);
-//         });
-//     });
-
-//     const actualBuff = Buffer.from((image as string).replace(/data:.*;base64,/, ''), 'base64');
-//     const actualPng = new PNG({width: testWidth, height: testHeight});
-//     actualPng.parse(actualBuff);
-
-//     const expectedPlatforms = ['ubuntu-runner', 'macos-runner', 'macos-local'];
-//     let minDiff = Infinity;
-//     for (const expected of expectedPlatforms) {
-//         const diff = compareByPixelmatch(actualPng, expected, testWidth, testHeight);
-//         if (diff < minDiff) {
-//             minDiff = diff;
-//         }
-//     }
-
-//     // At least one platform should be identical
-//     expect(minDiff).toBe(0);
-
-// }, 20000);
-// });
-
-// afterEach(async() => {
-//     await browser.close();
-// });
-
-// afterAll(async () => {
-//     if (server) {
-//         server.close();
-//     }
-// });
-
-//             function compareByPixelmatch(actualPng:PNG, platform: string, width:number, height:number): number {
-//                 const platformFixtureBase64 = fs.readFileSync(
-//                     path.join(__dirname, `fixtures/cjk-expected-base64-image/${platform}-base64.txt`), 'utf8')
-//                     .replace(/\s/g, '')
-//                     .replace(/data:.*;base64,/, '');
-
-//                 const expectedBuff = Buffer.from(platformFixtureBase64, 'base64');
-
-//                 const expectedPng = new PNG({width: testWidth, height: testHeight});
-//                 expectedPng.parse(expectedBuff);
-
-//                 const diffImg = new PNG({width, height});
-
-//                 const diff = pixelmatch(
-//                     actualPng.data, expectedPng.data, diffImg.data,
-//                     width, height, {threshold: 0}) / (width * height);
-
-//                 return diff;
-//             }
-// });
--- vitest.config.ts
+++ vitest.config.ts
@@ -3,10 +3,10 @@ import {defineConfig} from 'vite';
 
 export default defineConfig({
     test: {
+        name: 'Browser',
         globals: true,
         include: [
             'test/integration/browser/browser.test.ts'
-            // 'test/integration/render/render.test.ts'
         ],
     },
 });
